d1eab2aeb0cb0b153ab3f585abde9218a1b1b78b
pra2107tham <prathamshirbhate1909@gmail.com>
2025-04-08 15:09:22 +0530

    Update middleware and package dependencies
    - Added comments to `middleware.ts` for better clarity.
    - Removed outdated onboarding checks from the middleware logic.
    - Updated package dependencies in `package-lock.json` to the latest versions for `next-themes`, `@clerk/backend`, `@clerk/clerk-react`, `@clerk/shared`, and `@clerk/types`.

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+// middleware.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
@@ -6,23 +7,10 @@ const isPublicRoute = createRouteMatcher(['/', '/login', '/signup'])
 export default clerkMiddleware(async (auth, req) => {
   const { userId, sessionClaims, redirectToSignIn } = await auth()
 
-  // If the user isn't signed in and the route is private, redirect to sign-in
   if (!userId && !isPublicRoute(req)) {
     return redirectToSignIn({ returnBackUrl: req.url })
   }
 
-  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
-  // Redirect them to the /onboading route to complete onboarding
-  // if (
-  //   userId &&
-  //   !sessionClaims?.metadata?.onboardingComplete &&
-  //   req.nextUrl.pathname !== '/'
-  // ) {
-  //   const onboardingUrl = new URL('/', req.url)
-  //   return NextResponse.redirect(onboardingUrl)
-  // }
-
-  // If the user is logged in and the route is protected, let them view.
   if (userId && !isPublicRoute(req)) {
     return NextResponse.next()
   }
@@ -30,9 +18,11 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
+  unstable_allowDynamic: [
+    '/node_modules/@babel/runtime/regenerator/index.js',
+    '/node_modules/@clerk/nextjs/**/*.mjs',
+  ]
 }
